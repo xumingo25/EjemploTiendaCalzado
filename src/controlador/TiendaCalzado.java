23476e5fa075dd0b0401dbf4c572912bc558986b
Domingo <domingo.saavedra@tsoftglobal.com>
2023-04-10 12:24:35 -0400

    Se finaliza v1.algo del ejercicio de preparación para los contenidos de java del Bootcamp12. Se finalizan capas de control y vista. Se realizan smoke tests desde el pto de vista del desarrollador.

--- src/controlador/TiendaCalzado.java
+++ src/controlador/TiendaCalzado.java
@@ -25,7 +25,7 @@ public class TiendaCalzado {
         this.tienda = tienda;
     }
 
-    public TiendaCalzado(){
+    public TiendaCalzado() {
 
     }
 
@@ -37,10 +37,10 @@ public class TiendaCalzado {
     //{calzado1,calzado2,calzado3}
     //{AAAA    ,AA       ,  AAA}
     //{   0    ,   1    ,  2    }
-    public int buscarCalzado(String codigoCalzado){ //AAA
-        for(int i=0; i < tienda.size();i++){
+    public int buscarCalzado(String codigoCalzado) { //AAA
+        for (int i = 0; i < tienda.size(); i++) {
             //definir una condición para encontrar el calzado indicado
-            if(tienda.get(i).getProducto().getCodigo().compareToIgnoreCase(codigoCalzado)==0){
+            if (tienda.get(i).getProducto().getCodigo().compareToIgnoreCase(codigoCalzado) == 0) {
                 //encontro el objeto en la colección
                 return i; //2
             }
@@ -49,22 +49,124 @@ public class TiendaCalzado {
     }
 
     //Ingresar calzados de mujer
-    public void ingresarCalzadoMujer(Mujer mujer){
-        if(buscarCalzado(mujer.getProducto().getCodigo())==-1){
+    public void ingresarCalzadoMujer(Mujer mujer) {
+        if (buscarCalzado(mujer.getProducto().getCodigo()) == -1) {
             tienda.add(mujer);
         }
     }
 
-    public void ingresarCalzadoHombre(Hombre hombre){
-        if(buscarCalzado(hombre.getProducto().getCodigo())==-1){
+    public void ingresarCalzadoHombre(Hombre hombre) {
+        if (buscarCalzado(hombre.getProducto().getCodigo()) == -1) {
             tienda.add(hombre);
         }
     }
 
-    public void ingresarCalzadoDeportivo(Deportivo deportivo){
-        if(buscarCalzado(deportivo.getProducto().getCodigo())==-1){
+    public void ingresarCalzadoDeportivo(Deportivo deportivo) {
+        if (buscarCalzado(deportivo.getProducto().getCodigo()) == -1) {
             tienda.add(deportivo);
         }
     }
 
+
+    //2.	Mostrar sólo los calzado de mujer
+    //En opción 2, debe mostrar Día de venta, altura de taco y descuento.
+
+    //{calzado1  ,calzado2    ,calzado3}
+    //{Hombre    ,Mujer       ,  Mujer}
+    //{   0    ,   1    ,  2    }
+    public String calzadosMujer() {
+        String mensaje = "";
+        Mujer mujer = null;
+        int contador = 0;
+        for (int i = 0; i < tienda.size(); i++) { //iterar en la colección
+            if (tienda.get(i) instanceof Mujer) { //es un calzado de mujer
+                mujer = (Mujer) tienda.get(i);
+
+                mensaje = mensaje + "\n dia de venta: " + mujer.getDiaVenta() + ", altura de taco: " + mujer.getAlturaTaco() +
+                        " y descuento: $" + mujer.descuento();
+                contador++;
+            }
+
+        }
+
+        if (contador == 0) {
+            mensaje = "no hay calzados de mujer :/";
+        }
+        return mensaje;
+    }
+
+
+    //3.	Valor Venta
+    //En Valor Venta mostrará el valor Venta y número del calzado de uno en particular.
+    public String valorVenta(String codigo) {
+        String mensaje = "";
+        for (int i = 0; i < tienda.size(); i++) { //iterar en la colección
+            if (tienda.get(i).getProducto().getCodigo().compareToIgnoreCase(codigo) == 0) {
+                //lo encontre
+                return "El valor venta del calzado es: $" + tienda.get(i).valorVenta() + " y el nro del calzado es talla: " +
+                        tienda.get(i).getNumero();
+            }
+        }
+
+        return "El codigo " + codigo + " no esta asociado a ningun calzado :/";
+    }
+
+    //{calzado1  ,calzado2    ,calzado3  }
+    //{Hombre    ,Mujer       ,  Mujer   }
+    //{ 10000    , 1432423    ,  2234234 } valor
+    //{ 20       , 5          ,  6       } stock
+    public int calzadosTOP() {
+        int suma = 0;
+
+        for (int i = 0; i < tienda.size(); i++) { //iterar en la colección
+            if(tienda.get(i).valorVenta() > 80000){
+                suma = suma + tienda.get(i).getProducto().getStock();
+            }
+        }
+        return suma;
+    }
+
+
+    //5.	Total Impuesto Especifico
+    //En opción 5 mostrará el total de impuesto específico si se vendieran todos los calzados
+    public int totalImpuestoEspeficico(){
+        int total = 0;
+        Mujer mujer = null;
+        Hombre hombre = null;
+
+        for (int i = 0; i < tienda.size(); i++) { //iterar en la colección
+            if(tienda.get(i) instanceof Mujer){
+                mujer = (Mujer) tienda.get(i);
+                total = total + (mujer.impuestoEspecifico()*mujer.getProducto().getStock());
+
+            }else if(tienda.get(i) instanceof Hombre){
+                hombre = (Hombre) tienda.get(i);
+                total = total + (hombre.impuestoEspecifico()*hombre.getProducto().getStock());
+            }
+        }
+            return total;
+        }
+
+
+
+
+        //6.	Total descuento
+        //En Total descuento mostrará el total de descuento en toda la tienda
+        public int totalDescuentos(){
+            int total = 0;
+            Mujer mujer = null;
+            Hombre hombre = null;
+
+            for (int i = 0; i < tienda.size(); i++) { //iterar en la colección
+                if(tienda.get(i) instanceof Mujer){
+                    mujer = (Mujer) tienda.get(i);
+                    total = total + (mujer.descuento()*mujer.getProducto().getStock());
+
+                }else if(tienda.get(i) instanceof Hombre){
+                    hombre = (Hombre) tienda.get(i);
+                    total = total + (hombre.descuento()*hombre.getProducto().getStock());
+                }
+            }
+            return total;
+        }
 }
--- src/vista/Aplicacion.java
+++ src/vista/Aplicacion.java
@@ -1,10 +1,7 @@
 package vista;
 
 import controlador.TiendaCalzado;
-import modelo.Deportivo;
-import modelo.Hombre;
-import modelo.Mujer;
-import modelo.Producto;
+import modelo.*;
 
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -12,42 +9,141 @@ import java.util.Scanner;
 public class Aplicacion {
     public static void main(String[] args) {
         TiendaCalzado tienda = new TiendaCalzado(new ArrayList<>());
+        String codigo;
+        //Deportivo deportivo = new Deportivo (new Producto("123ABC",100,10000),44,"Fin de semana","Futbol","Cuero");
 
-        /*
-        Deportivo deportivo = new Deportivo (new Producto("123ABC",100,10000),44,"Fin de semana","Futbol","Cuero");
+        //Hombre hombre = new Hombre(new Producto("222AAA",5,10000), "Fin de semana",43,"verde");
 
-        Hombre hombre = new Hombre(new Producto("222AAA",10,200000), "Fin de semana",43,"VERDE");
+        //Mujer mujer = new Mujer(new Producto("33AAA",5,10000), "Fin de semana",36,"Negro",11);
 
-        Mujer mujer = new Mujer(new Producto("222AAA",10,200000), "Fin de semana",43,"Negro",12);;
-
-        tienda.ingresarCalzadoDeportivo(deportivo);
-        tienda.ingresarCalzadoHombre(hombre);
-        tienda.ingresarCalzadoMujer(mujer); */
+        //tienda.ingresarCalzadoDeportivo(deportivo);
+        //tienda.ingresarCalzadoHombre(hombre);
+        //tienda.ingresarCalzadoMujer(mujer);
 
         int opcion;
         do{
                 opcion = menu();
                 switch(opcion){
                     case 1:
-                        System.out.println("Ingreso Calzados");
+                        codigo = pedirCodigo(); //solicito el codigo
+
+                        if(tienda.buscarCalzado(codigo) == -1){
+                            //no existe
+                            System.out.println("Favor ingrese el stock del calzado");
+                            int stock = Leer.datoInt();
+
+                            System.out.println("Favor ingrese el valor base del calzado");
+                            int valorBase = Leer.datoInt();
+
+                            String diaVenta;
+                            do{ // itera siempre que la condición de true
+                                System.out.println("¿Favor ingrese dia venta (Semana o fin de semana) ?"); //Fin de semana
+                                diaVenta=Leer.dato();
+                            }while(diaVenta.compareToIgnoreCase("Semana") != 0 && //true
+                                    diaVenta.compareToIgnoreCase("Fin de semana") != 0); //false  ---false
+
+                            System.out.println("Favor ingrese el numero del calzado");
+                            int numero = Leer.datoInt();
+
+                            int respuesta;
+                            do{
+                                System.out.println("El calzado es:   1) Deportivo     2)Hombre      3)Mujer");
+                                System.out.println("Favor ingrese opción para continuar...");
+                                respuesta = Leer.datoInt(); //2
+                            }while(respuesta < 1 || respuesta > 3 );
+                            //false       ||  false   ---- false /se sale
+
+
+                            if(respuesta==1){
+                                //calzado deportivo
+                                System.out.println("Favor ingrese el tipo de deporte"); String tipoDeporte = Leer.dato();
+
+                                String material;
+                                do{ // itera siempre que la condición de true
+                                    System.out.println("¿Favor ingrese material (Cuero o Lona) ?"); //
+                                    material=Leer.dato();
+                                }while(material.compareToIgnoreCase("Cuero") != 0 && //true
+                                        material.compareToIgnoreCase("Lona") != 0); //false  ---false
+
+                                Producto prod = new Producto(codigo,stock,valorBase); //instacio el producto
+                                Deportivo deportivo = new Deportivo(prod,numero,diaVenta,tipoDeporte,material); //intancio el calzado deportivo
+
+                                tienda.ingresarCalzadoDeportivo(deportivo);
+
+                                System.out.println("Se agrega calzado deportivo con exito :D");
+
+                            }else{
+                                //Hombre o Mujer
+                                //pediremos el color
+
+                                System.out.println("Favor ingrese color del calzado..."); String color = Leer.dato();
+                                if(respuesta==2){
+                                    //hombre
+                                    tienda.ingresarCalzadoHombre(
+                                            new Hombre(new Producto(codigo,stock,valorBase),diaVenta,numero,color));
+                                    System.out.println("Se agrega calzado de hombre con exito :D");
+
+                                }else{
+                                    //mujer
+                                    System.out.println("Favor ingrese altura de taco"); int alturaTaco = Leer.datoInt();
+
+
+                                    tienda.ingresarCalzadoMujer(
+                                            new Mujer(
+                                                    new Producto(codigo,stock,valorBase),
+                                                    diaVenta,numero,color,alturaTaco));
+
+                                    System.out.println("Se agrega calzado de mujer con exito :D");
+
+                                }
+                            }
+
+                        }else{
+                            System.out.println("El calzado codigo "+ codigo + "ya esta registrado");
+                        }
+
                         break;
                     case 2:
-                        System.out.println("Mostrar Calzados Mujer");
+                        if(tienda.getTienda().size()==0){
+                            System.out.println("No hay calzados :/");
+                        }else{
+                            System.out.println(tienda.calzadosMujer());
+                        }
                         break;
                     case 3:
-                        System.out.println("Valor Venta");
+                        if(tienda.getTienda().size()==0){
+                            System.out.println("No hay calzados :/");
+                        }else{
+                            String codigoSolicitado;
+                            codigoSolicitado = pedirCodigo();
+
+                            System.out.println(tienda.valorVenta(codigoSolicitado));
+                        }
                         break;
                     case 4:
-                        System.out.println("Calzados top");
+                        if(tienda.getTienda().size()==0){
+                            System.out.println("No hay calzados :/");
+                        }else{
+                            System.out.println("Tienes "+ tienda.calzadosTOP() + " calzadop TOP (valor mayor a $80.000)");
+                        }
                         break;
                     case 5:
-                        System.out.println("Total Impuesto Especifico");
+                        if(tienda.getTienda().size()==0){
+                            System.out.println("No hay calzados :/");
+                        }else{
+                            System.out.println("Tienes un total de $"+tienda.totalImpuestoEspeficico() + " en impuestos especificos " +
+                                    "si se vendieran todos los calzados");
+                        }
                         break;
                     case 6:
-                        System.out.println("Total descuento");
+                        if(tienda.getTienda().size()==0){
+                            System.out.println("No hay calzados :/");
+                        }else{
+                            System.out.println("Tienes un total de $"+tienda.totalDescuentos() + " en descuentos " +
+                                    "si se vendieran todos los calzados");
+                        }
                         break;
                 }
-
             }while(opcion !=7); //repetir
 
 
